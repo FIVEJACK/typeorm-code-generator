f8e6bc8e63673e640663879c4760c600b88f38d0
Henry Kurniawan Wibowo <henry@fivejack.com>
2021-08-31 10:26:08 +0700

    Add prettier to format generated code

--- src/handlers/column/column-code-formatter.ts
+++ src/handlers/column/column-code-formatter.ts
@@ -1,8 +1,20 @@
 import { TypeormColumn } from './interfaces';
+import prettier from 'prettier';
 
 export class ColumnCodeFormatter {
     public static format(columns: TypeormColumn[]): string {
         const jsonString = JSON.stringify(columns);
-        return jsonString.replace(/"([^"]+)":/g, '$1:');
+        let formattedJson = jsonString.replace(/"([^"]+)":/g, '$1:');
+
+        return prettier.format(formattedJson, {
+            tabWidth: 4,
+            printWidth: 120,
+            parser: 'babel',
+            bracketSpacing: true,
+            jsxBracketSameLine: true,
+            singleQuote: true,
+            trailingComma: 'all',
+            semi: true,
+        });
     }
 }
